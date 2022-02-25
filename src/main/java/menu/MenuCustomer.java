9fc00374d4b2a6d2662d1fdc330d81a6483a9e10
Ingrid <ingridleichterr@gmail.com>
2022-02-25 17:53:45 +0200

    adding methods for validating name and phonenumber

--- src/main/java/menu/MenuCustomer.java
+++ src/main/java/menu/MenuCustomer.java
@@ -76,9 +76,31 @@ public class MenuCustomer {
 
         Customer customer = new Customer();
         System.out.println("Menu register new customer");
-        System.out.println("Type the full name of the customer");
+
+        //adding name and checking if its valid
+        String name = null;
+        boolean invalidName = true;
+        while(invalidName){
+            System.out.println("Type full name of the customer");
+            name=input.next();
+            boolean result = validateName(name);
+            if (result){
+                invalidName=false;
+            }
+        }
         customer.setName(input.next());
-        System.out.println("Type the phone number");
+
+        //phone number adding and checking if valid
+        String phoneNumber = null;
+        boolean invalidPhoneNum = true;
+        while(invalidPhoneNum){
+            System.out.println("Type the phone number");
+            phoneNumber= input.next();
+            boolean result = validatePhoneNumber(phoneNumber);
+            if (result){
+                invalidPhoneNum = false;
+            }
+        }
         customer.setPhoneNumber(input.next());
 
         //email adding and checking if valid
@@ -100,7 +122,14 @@ public class MenuCustomer {
 
     //method for checking if name is entered correctly
     private boolean validateName (String name){
-        return true;
+        //name can't contain special characters/numbers, length must be 3-20 characters
+        Pattern pattern = Pattern.compile("^[a-zA-Z]+[\\-'\\s]?[a-zA-Z ]{2,19}$");
+        Matcher matcher = pattern.matcher(name);
+
+        if (!matcher.matches()){
+            System.out.println("Entered name is incorrect. Please enter valid name!");
+        }
+        return matcher.matches();
     }
 
 
@@ -108,7 +137,21 @@ public class MenuCustomer {
     private boolean validateEmail (String email){
         Pattern pattern = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
         Matcher matcher = pattern.matcher(email);
-        System.out.println("Entered email is correct: " + matcher.matches());
+
+        if (!matcher.matches()){
+            System.out.println("Entered e-mail is incorrect. Please enter valid email!");
+        }
+        return matcher.matches();
+    }
+
+    //method for checking if phonenumber is entered correctly
+    private boolean validatePhoneNumber (String phoneNumber){
+        Pattern pattern = Pattern.compile("[0-9*#+() -]*");
+        Matcher matcher = pattern.matcher(phoneNumber);
+
+        if (!matcher.matches()){
+            System.out.println("Entered phone number is incorrect. Please enter valid phone number!");
+        }
         return matcher.matches();
     }
 }
